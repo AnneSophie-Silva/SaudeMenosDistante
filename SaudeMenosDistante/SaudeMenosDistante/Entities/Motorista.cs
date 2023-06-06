18c9ab4ba66b4b01e15ac0f235b60d2d6f417dcd
Anne Sophie Silva <132367143+AnneSophie-Silva@users.noreply.github.com>
2023-06-06 14:12:31 +0100

    Projeto SaudeMenosDistante

---
+++ SaudeMenosDistante/SaudeMenosDistante/Entities/Motorista.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaudeMenosDistante.Entities
+{
+    internal class Motorista : Colaborador
+    {
+        //Propriedades
+        public string Id { get; private set; }
+
+
+        //Construtores
+        public Motorista()
+        {
+
+        }
+
+        public Motorista(string id, string nome, string morada, string telemovel, string email, double hora, double valorPorHora) 
+            : base(id, nome, morada, telemovel, email, hora, valorPorHora)
+        {
+            Id = id;
+        }
+
+
+        //Método para cálculo do vencimento:
+        public override double Vencimento()
+        {
+            return Hora * ValorPorHora;
+        }
+
+
+
+    }
+}
